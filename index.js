49caa5a85d92f59e833a501ac393e7274fb6d86c
Sivaganesh Natarajavel <sivaganeshnatarajavel@gmail.com>
2024-03-15 15:15:20 +0530

    made changes for deployment

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const client = new MongoClient(url);
 await client.connect();
 console.log("Connected to Mongo");
 
-
 app.get("/", (req, res) => {
   res.send("<h1>Vanakkam da mapla</h1>");
 });
@@ -22,42 +21,51 @@ app.post("/post", express.json(), async (req, res) => {
 });
 
 app.post("/postmany", express.json(), async (req, res) => {
-    const getPost = req.body;
-    const sendMethod = await client
-     .db("CRUD")
-     .collection("data")
-     .insertMany(getPost);
-    res.send(sendMethod);
-})
+  const getPost = req.body;
+  const sendMethod = await client
+    .db("CRUD")
+    .collection("data")
+    .insertMany(getPost);
+  res.send(sendMethod);
+});
 
-app.get("/get",express.json(), async(req,res)=>{
-    const getMethod = await client
-   .db("CRUD")
-   .collection("data")
-   .find({})
-   .toArray();
-    res.send(getMethod);
-})
+app.get("/get", express.json(), async (req, res) => {
+  const getMethod = await client
+    .db("CRUD")
+    .collection("data")
+    .find({})
+    .toArray();
+  res.send(getMethod);
+});
 
-app.get("/getone/:id", async(req,res)=>{
-    const {id} = req.params;
-    const getMethod = await client.db("CRUD").collection("data").findOne({_id: new ObjectId(id)});
-    res.send(getMethod);
-})
+app.get("/getone/:id", async (req, res) => {
+  const { id } = req.params;
+  const getMethod = await client
+    .db("CRUD")
+    .collection("data")
+    .findOne({ _id: new ObjectId(id) });
+  res.send(getMethod);
+});
 
-app.put("/update/:id", express.json(), async(req,res)=>{
-    const {id} = req.params;
-    const getpost = req.body;
-    const updateMethod = await client.db("CRUD").collection("data").updateOne({_id: new ObjectId(id)},{$set:{name:getpost.name}});
-    res.send(updateMethod);
-})
+app.put("/update/:id", express.json(), async (req, res) => {
+  const { id } = req.params;
+  const getpost = req.body;
+  const updateMethod = await client
+    .db("CRUD")
+    .collection("data")
+    .updateOne({ _id: new ObjectId(id) }, { $set: { name: getpost.name } });
+  res.send(updateMethod);
+});
 
-app.delete("/delete/:id", async(req,res)=>{
-    const {id} = req.params;
-    const deleteMethod = await client.db("CRUD").collection("data").deleteOne({_id: new ObjectId(id)});
-    res.send(deleteMethod);
-})
+app.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteMethod = await client
+    .db("CRUD")
+    .collection("data")
+    .deleteOne({ _id: new ObjectId(id) });
+  res.send(deleteMethod);
+});
 
-app.listen(8080, () => {
-    console.log("listening on port 8080");
-  });
+app.listen(4000, () => {
+  console.log("listening on port 8080");
+});
